653784a0b9c3aa8f1304e46c74eb8381f30602e0
imanaevre <imanaev.re@icloud.com>
2025-12-05 09:43:13 +0300

    создал класс Movie

---
+++ recomandator3000.py
@@ -0,0 +1,41 @@
+class Movie:
+
+    def __init__(self, movie_id: int, title: str, genres: List[Genre],
+                 director: str, year: int, rating: float):
+        self._movie_id = movie_id
+        self._title = title
+        self._genres = genres
+        self._director = director
+        self._year = year
+        self._rating = rating
+
+    @property
+    def movie_id(self):
+        return self._movie_id
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def genres(self):
+        return self._genres
+
+    @property
+    def director(self):
+        return self._director
+
+    @property
+    def year(self):
+        return self._year
+
+    @property
+    def rating(self):
+        return self._rating
+
+    def __str__(self):
+        genres_str = ", ".join([g.value for g in self._genres])
+        return f"{self._title} ({self._year}) - {genres_str}, режиссер: {self._director}, рейтинг: {self._rating}"
+
+    def __repr__(self):
+        return f"Movie(id={self._movie_id}, title='{self._title}')"
