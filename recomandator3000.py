eb23a07d4cd925332d21cd4688671a8637e25e50
imanaevre <imanaev.re@icloud.com>
2025-12-05 23:28:18 +0300

    добавил класс User

--- recomandator3000.py
+++ recomandator3000.py
@@ -56,3 +56,51 @@ class Movie:
 
     def __repr__(self):
         return f"Movie(id={self._movie_id}, title='{self._title}')"
+
+# Класс Пользователь
+class User:
+    def __init__(self, user_id: int, name: str, password: str):
+        self._user_id = user_id
+        self._name = name
+        self._password = password
+        self._watched_movies: Dict[int, float] = {}
+        self._preferred_genres: List[Genre] = []
+
+    @property
+    def user_id(self):
+        return self._user_id
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def watched_movies(self):
+        return self._watched_movies.copy()
+
+    @property
+    def preferred_genres(self):
+        return self._preferred_genres.copy()
+
+    def check_password(self, password: str):
+        #проверка пароля
+        return self._password == password
+
+    def add_rating(self, movie_id: int, rating: float):
+        #оценка фильма
+        if 0 <= rating <= 10:
+            self._watched_movies[movie_id] = rating
+        else:
+            print("Оценка должна быть от 0 до 10!")
+
+    def set_preferred_genres(self, genres: List[Genre]):
+        self._preferred_genres = genres
+
+    def has_watched(self, movie_id: int):
+        return movie_id in self._watched_movies
+
+    def __str__(self):
+        return f"Пользователь: {self._name} (ID: {self._user_id})"
+
+    def __repr__(self):
+        return f"User(id={self._user_id}, name='{self._name}')"
